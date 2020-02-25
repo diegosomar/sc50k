696756b9cdbb8c4667351c1a486d7051ec7b4d16
Diego Ramos <diego@somarcomunicacao.com.br>
2020-02-25 18:30:46 -0300

    Fix - Compatibility link rel preload with Firefox

--- assets/scripts/partials/common.js
+++ assets/scripts/partials/common.js
@@ -4,6 +4,14 @@
   $(function() {
     $(document).ready(function(){
 
+      // Enable firefox compatibility with link rel preload
+      if ( navigator.userAgent.search("Firefox") > -1 ) {
+        $('link[rel="preload"]').each(function(){
+          if ( $(this).attr('as') == 'style' ){
+            $(this).attr('rel', 'stylesheet');
+          }
+        });
+      }
     });
   });
 })(jQuery);
